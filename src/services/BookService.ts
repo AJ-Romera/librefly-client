16ab9817d78392fd2ca33b680ba255ec4be4bf5e
AJ Romera <ajromeraalfonso@gmail.com>
2023-03-31 19:48:43 +0200

    CHNG: Makes BookService to Fetch Data from the API REST

---
+++ src/services/BookService.ts
@@ -0,0 +1,6 @@
+import axios from "axios";
+import { Book } from "../ts-interfaces/Book.interfaces";
+
+export const getBooks = async () => {
+  return await axios.get("http://localhost:8080/books");
+};
