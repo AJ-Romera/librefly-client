46dc88a65d1ec96616d276655d353839c0121e31
AJ Romera <ajromeraalfonso@gmail.com>
2023-03-31 12:51:41 +0200

    CHNG: Organize the Pages and Routes

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <h1 className="text-3xl font-bold underline">LibreFly</h1>
+    <BrowserRouter>
+      {/* navbar */}
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   </React.StrictMode>
 );
