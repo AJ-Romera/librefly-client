fa055839954b1b30188ca3dcc39b7af60ac310e9
AJ Romera <ajromeraalfonso@gmail.com>
2023-03-31 12:25:36 +0200

    CHNG: Installs, Config and Test Tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <h1>LibreFly</h1>
+    <h1 className="text-3xl font-bold underline">LibreFly</h1>
   </React.StrictMode>
 );
