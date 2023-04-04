29f5dfc0591c120dfca7b22999b2d66d6bc6ce0b
AJ Romera <ajromeraalfonso@gmail.com>
2023-04-04 11:39:54 +0200

    CHNG: Makes all the BookServices to Fetch API Data

--- src/services/BookService.ts
+++ src/services/BookService.ts
@@ -1,6 +1,24 @@
 import axios from "axios";
 import { Book } from "../ts-interfaces/Book.interfaces";
 
+const API = "http://localhost:8080";
+
 export const getBooks = async () => {
-  return await axios.get<Book[]>("http://localhost:8080/books");
+  return await axios.get<Book[]>(`${API}/books`);
+};
+
+export const getBook = async (id: string) => {
+  return await axios.get<Book>(`${API}/books/${id}`);
+};
+
+export const createVideo = async (book: Book) => {
+  return await axios.post(`${API}/books`, book);
+};
+
+export const updateBook = async (id: string, book: Book) => {
+  return await axios.put<Book>(`${API}/books/${id}`, book);
+};
+
+export const deleteBook = async (id: string) => {
+  return await axios.delete<Book>(`${API}/books/${id}`);
 };
