b1dcb57c76ce554b409bc3a6a790cd226e4885e8
AJ Romera <ajromeraalfonso@gmail.com>
2023-04-11 13:46:30 +0200

    CHNG: Creates a Responsive Hero Section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import BookList from "../components/BookList";
+import HeroSection from "../components/HeroSection";
 
 const Home = () => {
   return (
     <div>
+      <HeroSection />
       <BookList />
     </div>
   );
