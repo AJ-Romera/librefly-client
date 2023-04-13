95ddd18f5838cb6b54a734ab1c7d9e33caa73fb0
AJ Romera <ajromeraalfonso@gmail.com>
2023-04-13 18:33:58 +0200

    CHNG: Styles the BookForm Properly and Adds Responsiveness & Minor mt Change

--- src/components/BookForm.tsx
+++ src/components/BookForm.tsx
@@ -57,75 +57,104 @@ const BookForm = () => {
   }, []);
 
   return (
-    <div className="flex justify-center items-center h-screen -mt-20 w-full">
-      <div className="block max-w-sm rounded-lg bg-white shadow-lg dark:bg-neutral-700 w-1/2">
-        <div className="p-6">
-          {params.id ? <h3>Edit Book</h3> : <h3>New Book</h3>}
-          <form id="form" onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="book-title">Book Title</label>
-              <input
-                id="book-title"
-                type="text"
-                name="name"
-                value={book.name}
-                placeholder="Write a title for the book"
-                autoFocus
-                required
-                onChange={handleInputChange}
-              />
-            </div>
-
-            <div>
-              <label htmlFor="isbn">ISBN</label>
-              <input
-                id="isbn"
-                type="text"
-                name="isbn"
-                value={book.isbn}
-                placeholder="Write the ISBN of the book"
-                required
-                onChange={handleInputChange}
-              />
-            </div>
-
-            <div>
-              <label htmlFor="author_first_name">Author's first name</label>
-              <input
-                id="author_first_name"
-                type="text"
-                name="author_first_name"
-                value={book.author_first_name}
-                placeholder="Write the first name of the author"
-                required
-                onChange={handleInputChange}
-              />
-            </div>
-
-            <div>
-              <label htmlFor="author_last_name">Author's last name</label>
-              <input
-                id="author_last_name"
-                type="text"
-                name="author_last_name"
-                value={book.author_last_name}
-                placeholder="Write the last name of the author"
-                required
-                onChange={handleInputChange}
-              />
-            </div>
-
-            {params.id ? (
-              <button className="bg-green-500 inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]">
-                Update
-              </button>
-            ) : (
-              <button className="bg-green-700 inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]">
-                Create
-              </button>
-            )}
-          </form>
-        </div>
+    <div className="flex justify-center items-center h-screen w-full">
+      <div className="container mx-auto py-8">
+        <h2 className="text-2xl font-bold mb-6 text-center">
+          {params.id ? <h3>Edit Book</h3> : <h3>Publish a Book</h3>}
+        </h2>
+        <form
+          className="w-full max-w-sm mx-auto p-8 rounded-md shadow-md bg-blue-300/30"
+          id="form"
+          onSubmit={handleSubmit}
+        >
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="book-title"
+            >
+              Book Title
+            </label>
+            <input
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              id="book-title"
+              type="text"
+              name="name"
+              value={book.name}
+              placeholder="Write a title for the book"
+              autoFocus
+              required
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="isbn"
+            >
+              ISBN
+            </label>
+            <input
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              id="isbn"
+              type="text"
+              name="isbn"
+              value={book.isbn}
+              placeholder="Write the ISBN of the book"
+              required
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="author_first_name"
+            >
+              Author's First Name
+            </label>
+            <input
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              type="text"
+              name="author_first_name"
+              value={book.author_first_name}
+              placeholder="Write the first name of the author"
+              required
+              onChange={handleInputChange}
+            />
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="author_last_name"
+            >
+              Author's Last Name
+            </label>
+            <input
+              className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              id="author_last_name"
+              type="text"
+              name="author_last_name"
+              value={book.author_last_name}
+              placeholder="Write the last name of the author"
+              required
+              onChange={handleInputChange}
+            />
+          </div>
+          {params.id ? (
+            <button className="bg-green-500 inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]">
+              Update
+            </button>
+          ) : (
+            <button className="bg-green-700 inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]">
+              Create
+            </button>
+          )}
+          {/* <button
+            className="w-full bg-indigo-500 text-white text-sm font-bold py-2 px-4 rounded-md hover:bg-indigo-600 transition duration-300"
+            type="submit"
+          >
+            Register
+          </button> */}
+        </form>
       </div>
     </div>
   );
--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -17,7 +17,7 @@ const BookList = () => {
   }, []);
 
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 w-full mx-auto gap-3 my-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 w-full mx-auto gap-3 my-6 mb-12">
       {books.map((book) => {
         return <BookItem book={book} key={book._id} loadBooks={loadBooks} />;
       })}
