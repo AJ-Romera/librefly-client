2a9c516c2628611737e5e1a03372e48d2b3e3f13
AJ Romera <ajromeraalfonso@gmail.com>
2023-04-14 16:26:59 +0200

    CHNG: Adds Helper to ScrollToTop when Changing Routes

--- src/main.tsx
+++ src/main.tsx
@@ -10,12 +10,14 @@ import Navbar from "./components/Navbar";
 import CreateBook from "./pages/CreateBook";
 import DetailsPage from "./pages/DetailsPage";
 import Footer from "./components/Footer";
+import ScrollToTop from "./helpers/ScrollToTop";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <Navbar />
       <div className="max-w-[1640px] mx-auto px-4">
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new-book" element={<CreateBook />} />
