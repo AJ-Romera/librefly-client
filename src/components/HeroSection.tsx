836b6ff98d52e9c2b155fb6d737e54b418818350
AJ Romera <ajromeraalfonso@gmail.com>
2023-04-14 14:18:58 +0200

    CHNG: Completely Reworks HeroSection

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,46 +1,50 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const HeroSection = () => {
   return (
     <section>
-      <div className="bg-blue-400 text-white py-6 mt-6">
-        <div className="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
-          <div className="flex flex-col w-full lg:w-1/3 justify-center items-start p-8">
-            <h1 className="text-3xl md:text-5xl p-2 text-yellow-300 tracking-loose">
-              TechFest
-            </h1>
-            <h2 className="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">
-              Space : The Timeless Infinity
-            </h2>
-            <p className="text-sm md:text-base text-gray-50 mb-4">
-              Explore your favourite events and register now to showcase your
-              talent and win exciting prizes.
-            </p>
-            <a
-              href="#"
-              className="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent"
-            >
-              Explore Now
-            </a>
-          </div>
-          <div className="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3  justify-center">
-            <div className="h-48 flex flex-wrap content-center">
+      <div className="bg-hero-1 text-white">
+        <div className="bg-gradient-to-r from-white to-blue-500/40 backdrop-blur-sm py-6 mt-6">
+          <div className="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
+            <div className="flex flex-col w-full lg:w-1/3 justify-center items-start py-8 pl-8">
+              <h1 className="text-3xl md:text-5xl py-2 text-blue-500 tracking-loose">
+                LibreFly
+              </h1>
+              <h2 className="text-3xl md:text-4xl leading-relaxed md:leading-snug mb-2 text-slate-800">
+                Experience the joy of reading and let your thoughts take flight
+                with LibreFly
+              </h2>
+              <p className="text-sm md:text-base text-slate-600 mb-4">
+                Feed your mind and nourish your soul with our extensive
+                collection of books. Or bring your book to life and connect with
+                readers who will love and cherish your work
+              </p>
+              <Link
+                to="/new-book"
+                className="bg-transparent hover:bg-blue-400 text-blue-400 hover:text-black rounded shadow hover:shadow-lg py-2 px-7 md:py-2 md:px-4 border border-blue-400 hover:border-transparent"
+              >
+                Publish Now
+              </Link>
+            </div>
+
+            <div className="hidden m-28 w-full gap-1 content-center items-center justify-center md:flex flex-wrap md:h-48">
               <div>
                 <img
-                  className="mt-28 hidden xl:block"
-                  src="https://user-images.githubusercontent.com/54521023/116969935-c13d5b00-acd4-11eb-82b1-5ad2ff10fb76.png"
+                  className="w-60 mt-14 hidden xl:block"
+                  src="https://images-eu.bookshop.org/es/9788499083209.jpg?height=500&v=v4-eee5a36253261bb63539fd94d0fe7cca"
                 />
               </div>
               <div>
                 <img
-                  className="inline-block mt-24 md:mt-0 p-8 md:p-0"
-                  src="https://user-images.githubusercontent.com/54521023/116969931-bedb0100-acd4-11eb-99a9-ff5e0ee9f31f.png"
+                  className="w-60 inline-block mt-24 md:mt-0 p-8 md:p-0"
+                  src="https://images-eu.bookshop.org/es/9788418037085.jpg?height=500&v=v4-aa52b52cb90b45ea379a1769cb1589a8"
                 />
               </div>
               <div>
                 <img
-                  className="mt-28 hidden lg:block"
-                  src="https://user-images.githubusercontent.com/54521023/116969939-c1d5f180-acd4-11eb-8ad4-9ab9143bdb50.png"
+                  className="w-60 mt-28 hidden lg:block"
+                  src="https://images-eu.bookshop.org/es/9788412553079.jpg?height=500&v=v4-8f31471954da34088565abbb6ccb9c06"
                 />
               </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "hero-1":
+          "url('https://images.unsplash.com/photo-1481121749114-c5488c93b0d1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1172&q=80')",
+      },
+    },
   },
   plugins: [],
 };
