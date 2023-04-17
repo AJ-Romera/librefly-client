45b34fdaf6eb3bfe96b30285090308172ebcf176
AJ Romera <ajromeraalfonso@gmail.com>
2023-04-17 17:37:18 +0200

    CHNG: Adds All the Form Imputs, Selects, etc of a Book

--- src/components/BookForm.tsx
+++ src/components/BookForm.tsx
@@ -5,6 +5,7 @@ import { toast } from "react-toastify";
 import { useNavigate, useParams } from "react-router-dom";
 
 type InputChange = ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
+type SelectChange = ChangeEvent<HTMLSelectElement>;
 type FormChange = FormEvent<HTMLFormElement>;
 
 const BookForm = () => {
@@ -16,9 +17,14 @@ const BookForm = () => {
     isbn: "",
     author_first_name: "",
     author_last_name: "",
+    cover: "",
   });
 
   const handleInputChange = (e: InputChange) => {
+    setBook({ ...book, [e.target.name]: e.target.value });
+  };
+
+  const handleSelectChange = (e: SelectChange) => {
     setBook({ ...book, [e.target.name]: e.target.value });
   };
 
@@ -43,8 +49,34 @@ const BookForm = () => {
   const getBook = async (id: string) => {
     try {
       const response = await bookService.getBook(id);
-      const { name, isbn, author_first_name, author_last_name } = response.data;
-      setBook({ name, isbn, author_first_name, author_last_name });
+      const {
+        name,
+        isbn,
+        author_first_name,
+        author_last_name,
+        cover,
+        blurb,
+        genre,
+        pages,
+        publisher,
+        binding,
+        year_of_publication,
+        release_date,
+      } = response.data;
+      setBook({
+        name,
+        isbn,
+        author_first_name,
+        author_last_name,
+        cover,
+        blurb,
+        genre,
+        pages,
+        publisher,
+        binding,
+        year_of_publication,
+        release_date,
+      });
     } catch (error) {
       console.error({ message: "Error: No book found with the given id" });
     }
@@ -57,13 +89,13 @@ const BookForm = () => {
   }, []);
 
   return (
-    <div className="flex justify-center items-center h-screen w-full">
+    <div className="flex justify-center items-center h-full w-full">
       <div className="container mx-auto py-8">
         <h2 className="text-2xl font-bold mb-6 text-center">
-          {params.id ? <h3>Edit Book</h3> : <h3>Publish a Book</h3>}
+          {params.id ? "Edit Book" : "Publish a Book"}
         </h2>
         <form
-          className="w-full max-w-sm mx-auto p-8 rounded-md shadow-md bg-blue-300/30"
+          className="w-full max-w-sm lg:max-w-2xl mx-auto p-8 rounded-md shadow-md bg-blue-300/30"
           id="form"
           onSubmit={handleSubmit}
         >
@@ -72,7 +104,7 @@ const BookForm = () => {
               className="block text-gray-700 text-sm font-bold mb-2"
               htmlFor="book-title"
             >
-              Book Title
+              Book Title (*)
             </label>
             <input
               className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
@@ -86,12 +118,13 @@ const BookForm = () => {
               onChange={handleInputChange}
             />
           </div>
+
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
               htmlFor="isbn"
             >
-              ISBN
+              ISBN (*)
             </label>
             <input
               className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
@@ -104,41 +137,226 @@ const BookForm = () => {
               onChange={handleInputChange}
             />
           </div>
+
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="author_first_name"
+              >
+                Author's First Name (*)
+              </label>
+              <input
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                type="text"
+                name="author_first_name"
+                value={book.author_first_name}
+                placeholder="Write the first name of the author"
+                required
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="w-full md:w-1/2 px-3">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="author_last_name"
+              >
+                Author's Last Name (*)
+              </label>
+              <input
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                id="author_last_name"
+                type="text"
+                name="author_last_name"
+                value={book.author_last_name}
+                placeholder="Write the last name of the author"
+                required
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="author_first_name"
+              htmlFor="cover"
             >
-              Author's First Name
+              Book´s Cover (*)
             </label>
             <input
               className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              id="cover"
               type="text"
-              name="author_first_name"
-              value={book.author_first_name}
-              placeholder="Write the first name of the author"
+              name="cover"
+              value={book.cover}
+              placeholder="Cover of the book"
               required
               onChange={handleInputChange}
             />
           </div>
+
+          <span className="flex items-center justify-center text-gray-400 text-xs font-semibold pt-4">
+            --- OPTIONAL ---
+          </span>
+
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="author_last_name"
+              htmlFor="blurb"
             >
-              Author's Last Name
+              Blurb
             </label>
-            <input
+            <textarea
               className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
-              id="author_last_name"
-              type="text"
-              name="author_last_name"
-              value={book.author_last_name}
-              placeholder="Write the last name of the author"
-              required
+              id="blurb"
+              rows={3}
+              name="blurb"
+              value={book.blurb}
+              placeholder="Blurb of the book"
               onChange={handleInputChange}
             />
           </div>
+
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full md:w-2/12 px-3 mb-6 md:mb-0">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="pages"
+              >
+                Pages
+              </label>
+              <input
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                id="pages"
+                type="number"
+                name="pages"
+                value={book.pages}
+                placeholder="100"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="w-full md:w-10/12 px-3 mb-6 md:mb-0">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="genre"
+              >
+                Genre
+              </label>
+              <div className="relative">
+                <select
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500 focus:bg-white"
+                  id="genre"
+                  name="genre"
+                  value={book.genre}
+                  placeholder="Genre of the book"
+                  onChange={handleSelectChange}
+                >
+                  <option>Action and Adventure</option>
+                  <option>Classics</option>
+                  <option>Comic Book or Graphic Novel</option>
+                  <option>Detective and Mystery</option>
+                  <option>Fantasy</option>
+                  <option>Historical Fiction</option>
+                  <option>Horror</option>
+                  <option>Literary Fiction</option>
+                  <option>Romance</option>
+                  <option>Science Fiction (Sci-Fi)</option>
+                  <option>Short Stories</option>
+                  <option>Suspense and Thrillers</option>
+                  <option>Women's Fiction</option>
+                  <option>Biographies and Autobiographies</option>
+                  <option>Cookbooks</option>
+                  <option>Essays</option>
+                  <option>History</option>
+                  <option>Memoir</option>
+                  <option>Poetry</option>
+                  <option>Self-Help</option>
+                  <option>True Crime</option>
+                </select>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="binding"
+              >
+                Binding
+              </label>
+              <div className="relative">
+                <select
+                  className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500 focus:bg-white"
+                  id="binding"
+                  name="binding"
+                  value={book.binding}
+                  placeholder="Binding of the book"
+                  onChange={handleSelectChange}
+                >
+                  <option>Hard Binding</option>
+                  <option>Soft Binding</option>
+                  <option>Pocket Edition</option>
+                  <option>Ebook</option>
+                </select>
+              </div>
+            </div>
+            <div className="w-full md:w-1/2 px-3">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="publisher"
+              >
+                Publisher
+              </label>
+              <input
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                id="publisher"
+                type="text"
+                name="publisher"
+                value={book.publisher}
+                placeholder="Publisher of the book"
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full md:w-2/6 px-3 mb-6 md:mb-0">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="year_of_publication"
+              >
+                Publication Year
+              </label>
+              <input
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                id="year_of_publication"
+                type="number"
+                name="year_of_publication"
+                value={book.year_of_publication}
+                placeholder="2023"
+                onChange={handleInputChange}
+              />
+            </div>
+            <div className="w-full md:w-2/3 px-3 mb-6 md:mb-0">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="release_date"
+              >
+                Release Date
+              </label>
+              <input
+                className="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+                id="release_date"
+                type="text"
+                name="release_date"
+                value={book.release_date}
+                placeholder="Release Date of the Book"
+                onChange={handleInputChange}
+              />
+            </div>
+          </div>
+
           {params.id ? (
             <button className="bg-green-500 inline-block rounded bg-primary px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]">
               Update
@@ -148,12 +366,6 @@ const BookForm = () => {
               Create
             </button>
           )}
-          {/* <button
-            className="w-full bg-indigo-500 text-white text-sm font-bold py-2 px-4 rounded-md hover:bg-indigo-600 transition duration-300"
-            type="submit"
-          >
-            Register
-          </button> */}
         </form>
       </div>
     </div>
--- src/ts-interfaces/Book.interfaces.ts
+++ src/ts-interfaces/Book.interfaces.ts
@@ -3,6 +3,14 @@ export interface Book {
   isbn: string;
   author_first_name: string;
   author_last_name: string;
+  cover: string;
+  blurb?: string;
+  genre?: string;
+  pages?: number;
+  publisher?: string;
+  binding?: string;
+  year_of_publication?: number;
+  release_date?: string;
   _id?: string;
   createdAt?: string;
   updatedAt?: string;
